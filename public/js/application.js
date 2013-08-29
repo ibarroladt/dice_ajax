25cd71ba7c26e153ced1eb78e25c90473932285e
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 22:07:59 -0500

    final commit

--- public/js/application.js
+++ public/js/application.js
@@ -6,5 +6,12 @@ $(document).ready(function () {
   //   3- generate a random number between 1 and 6 using JavaScript
   //   4- use jQuery to submit an AJAX post to the form's action
   //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
-
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var url = $('form').attr('action');
+    var val = 1 + Math.floor(Math.random() * 6);
+    $.post(url, {value: val}, function(data, status, server){
+      $('#die').replaceWith(data);
+    }); 
+  });
 });
